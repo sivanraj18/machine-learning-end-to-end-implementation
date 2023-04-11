ca8e289c9f17b2c18f3ac6fc4e800eaaae7179ab
sivanraj18 <boyinasivanagaraju5@gmail.com>
2023-04-11 13:08:36 +0530

    splitted_data

--- src/split_data.py
+++ src/split_data.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 import yaml
-import pandas
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from get_data import read_params
 
@@ -15,11 +15,16 @@ def split_data(config_path):
     plit_ratio = config['split_data']['test_size']
 
     df = pd.read_csv(dataset,sep = ',')
-    
+    train_df ,test_df = train_test_split(df,random_state=random_state,test_size = plit_ratio)
+    train_df.to_csv(train_data,index = False)
+    test_df.to_csv(test_data,index = False)
+    df.to_csv(dataset,index = False)
+
+
 
 
 if __name__ == "__main__":
     arg_parse = argparse.ArgumentParser()
     arg_parse.add_argument('--config',default = 'params.yaml')
     agr = arg_parse.parse_args()
-    split_data(config_path=arg.config)
+    split_data(config_path=agr.config)

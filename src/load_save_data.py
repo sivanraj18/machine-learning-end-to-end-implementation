2544d3e8bca5bbc8126ea8002650586549630e09
sivanraj18 <boyinasivanagaraju5@gmail.com>
2023-04-11 13:11:51 +0530

    modified load_data.py

--- src/load_save_data.py
+++ src/load_save_data.py
@@ -4,13 +4,11 @@ import yaml
 import numpy
 from get_data import read_params,get_data
 import argparse
-from sklearn.model_selection import train_test_split
 
 def load_save(config_path):
     config = read_params(config_path)
     df = get_data(config_path)
     dataset = config['load_data']['raw_dataset_csv']
-    train_df,test_df = train_test_split(df,random_state=rndm_st,test_size=size)
     df.to_csv(dataset,index = False)
 
 if __name__ == "__main__":
